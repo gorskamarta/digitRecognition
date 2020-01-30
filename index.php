764c83e89b756d6634a706b13ad496c2a0c9cdc2
mgorska <marta.gorska@primesoft.pl>
2020-01-30 03:52:57 +0100

    Refactor

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <h1>Draw a digit</h1>
     <canvas id="toRecognize" width="280px" height="280px" style="border: solid;black"></canvas>
+    <br>
     <button id="send">Send</button>
     <button id="clear">Clear</button>
     <pre id="response"></pre>
